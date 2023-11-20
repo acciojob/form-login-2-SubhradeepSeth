7c08ce366d2b9c9e0931874a2689d4267ad4994d
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-11-20 21:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function myFunction(){
+    var firstName = document.getElementById('f_name').value;
+    var lastName = document.getElementById('l-name').value;
+    var phoneNumber = document.getElementById('ph-number').value;
+    var email = document.getElementById('email').value;
+
+    var alertMessage = "First Name: " + firstName + "\n" +
+                       "Last Name: " + lastName + "\n" +
+                       "Phone Number: " + phoneNumber + "\n" +
+                       "Email ID: " + email;
+
+    alert(alertMessage);
+}
